35198db65ffb22530d52e37168924bf8a5ba82d6
kevinmoore444 <kevinmoore444@gmail.com>
2023-02-13 14:51:44 -0800

    commit

---
+++ JavaFirstClass.java
@@ -0,0 +1,5 @@
+public class JavaFirstClass {
+	public static void main(String[] args) {
+        System.out.println("My name is Kevin Moore. I am 36 years old. My Hometown is San Bruno, California");
+	}
+}
